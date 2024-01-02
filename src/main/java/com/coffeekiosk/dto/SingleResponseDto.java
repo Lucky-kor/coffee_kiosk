1d61d9bd45c691db1859dca15522c0410d1b2cb0
jungminLee <jungmin2056@gmail.com>
2024-01-02 13:53:38 +0900

    Feature member

---
+++ src/main/java/com/coffeekiosk/dto/SingleResponseDto.java
@@ -0,0 +1,10 @@
+package com.coffeekiosk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+}
---
+++ src/main/java/com/coffeekiosk/stamp/Stamp.java
@@ -0,0 +1,26 @@
+package com.coffeekiosk.stamp;
+
+import com.coffeekiosk.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Stamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stampId;
+
+    @Column(nullable = false)
+    private int stampCount;
+
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+}
