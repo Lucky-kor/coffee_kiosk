01099f28b98742bc118da477d8b18c0c5d8a6620
jungminLee <jungmin2056@gmail.com>
2024-01-02 10:45:58 +0900

    init project

---
+++ src/main/java/com/coffeekiosk/CoffeeKioskStartApplication.java
@@ -0,0 +1,11 @@
+package com.coffeekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeKioskStartApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CoffeeKioskStartApplication.class, args);
+  }
+}
---
+++ src/test/java/com/coffeekiosk/CoffeeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
